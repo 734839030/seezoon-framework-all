17c9e7c51c81aa788e8159b75bde4c7ad7dd5b6e
734839030 <734839030@qq.com>
2018-06-03 22:50:22 +0800

    升级驱动支持 支持mysql 8

--- db/seezoon-framework0604.sql
+++ db/seezoon-framework0604.sql
@@ -58,7 +58,7 @@ DROP TABLE IF EXISTS `sys_dept`;
 CREATE TABLE `sys_dept` (
   `id` varchar(32) NOT NULL COMMENT '编号',
   `parent_id` varchar(32) NOT NULL COMMENT '父部门',
-  `parent_ids` varchar(2000) NOT NULL COMMENT '父ids，按层级逗号分隔',
+  `parent_ids` varchar(500) NOT NULL COMMENT '父ids，按层级逗号分隔',
   `name` varchar(100) NOT NULL COMMENT '部门名称',
   `sort` int(10) NOT NULL COMMENT '排序',
   `create_by` varchar(32) NOT NULL COMMENT '创建者',
@@ -68,7 +68,6 @@ CREATE TABLE `sys_dept` (
   `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
   PRIMARY KEY (`id`),
   KEY `parent_id` (`parent_id`),
-  KEY `parent_ids` (`parent_ids`(255)),
   KEY `create_date` (`create_date`),
   KEY `create_by` (`create_by`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT COMMENT='组织机构';
