b2a4e1ef8ed311e43097de9d55df2fec07a8a13b
hdf <hdf@qq.com>
2018-12-02 22:19:25 +0800

    up

---
+++ src/test/java/com/seezoon/framework/modules/system/SysParamServiceTest.java
@@ -0,0 +1,18 @@
+package com.seezoon.framework.modules.system;
+
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.seezoon.framework.common.context.test.BaseJunitTest;
+import com.seezoon.framework.modules.system.service.SysParamService;
+
+public class SysParamServiceTest extends BaseJunitTest{
+
+	@Autowired
+	private SysParamService sysParamService;
+	@Test
+	public void testSave() {
+	}
+
+}
